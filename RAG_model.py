72c08d6a10810625f2d7b1748408dd0b1801f3cd
Claire Chen <71817290+seernono2001@users.noreply.github.com>
2026-03-19 16:51:28 -0400

    Refactor RAG model code for clarity and updates

--- RAG_model.py
+++ RAG_model.py
@@ -2,6 +2,8 @@ from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain_classic.chains import RetrievalQA
+# from langchain_openai import ChatOpenAI            //If we're back using ChatGPT
+
 
 # Build retriever
 embeddings = HuggingFaceEmbeddings(model_name="paraphrase-multilingual-MiniLM-L12-v2")
@@ -10,6 +12,7 @@ retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
 
 # Build RAG chain
 llm = ChatGroq(model="llama-3.3-70b-versatile", api_key="")
+# llm = ChatOpenAI(model="gpt-4o", api_key="sk-xxxxxxxxxxxxxxxx")      //If we're back using ChatGPT
 qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
 detected_foods = ["chicken", "rice", "vegetables"]
 
